29d04bc958e72fb61b9c38c4244e075e46457288
WuttichaiPass <Wuttichaiamon2000@gmail.com>
2023-02-16 16:52:20 +0700

    Favourites Bar

--- src/features/restaurants/screens/restaurants.screen.jsx
+++ src/features/restaurants/screens/restaurants.screen.jsx
@@ -1,6 +1,5 @@
-import { StatusBar, FlatList } from "react-native";
-import React, { useState, useContext } from "react";
-import { Searchbar } from "react-native-paper";
+import { FlatList, TouchableOpacity } from "react-native";
+import React, { useContext, useState } from "react";
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
 import styled from "styled-components/native";
 import { Spacer } from "../../../components/spacer/spacer.component";
@@ -8,9 +7,8 @@ import { SafeArea } from "../../../components/utility/safe-area.component";
 import { RestaurantsContext } from "../../../services/restaurants/restaurants.context";
 import { ActivityIndicator, MD2Colors } from "react-native-paper";
 import { Search } from "../components/search.component";
-
-
-
+import { FavouritesBar } from "../../../components/favourites/favourites-bar.component";
+import { FavouritesContext } from "../../../services/favourites/favourites.context";
 
 const RestaurantList = styled(FlatList).attrs({
     contentContainerStyle: {
@@ -28,13 +26,14 @@ top: 50%;
 left: 50%;
 `;
 
-export const RestaurantsScreen = () => {
+export const RestaurantsScreen = ({ navigation }) => {
     const [searchQuery, setSearchQuery] = useState("");
     const onChangeSearch = (query) => setSearchQuery(query);
 
     const { isLoading, error, restaurants } =
         useContext(RestaurantsContext);
-    console.log(error);
+    const [isToggled, setIsToggled] = useState(false);
+    const { favourites } = useContext(FavouritesContext);
 
     return (
         <SafeArea>
@@ -44,14 +43,22 @@ export const RestaurantsScreen = () => {
                         color={MD2Colors.purple900} />
                 </LoadingContainer>
             )}
-            <Search />
+            <Search isFavouritesToggled={isToggled}
+                onFavouritesToggle={() => setIsToggled(!isToggled)} />
+            {isToggled && (
+                <FavouritesBar favourites={favourites}
+                    onNavigate={navigation.navigate} />
+            )}
             <RestaurantList
                 data={restaurants}
                 renderItem={({ item }) => {
                     return (
-                        <Spacer position="bottom" size="large">
-                            <RestaurantInfoCard restaurant={item} />
-                        </Spacer>
+                        <TouchableOpacity onPress={() =>
+                            navigation.navigate("RestaurantDetail", { restaurant: item })}>
+                            <Spacer position="bottom" size="large">
+                                <RestaurantInfoCard restaurant={item} />
+                            </Spacer>
+                        </TouchableOpacity>
                     );
                 }}
                 keyExtractor={(item) => item.name}
