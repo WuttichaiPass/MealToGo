8aff5994c2f3dc00c5ebc3f9134a2611d3f28356
WuttichaiPass <Wuttichaiamon2000@gmail.com>
2023-02-16 16:39:29 +0700

    FlatList and Navigation

--- src/components/utility/safe-area.component.jsx
+++ src/components/utility/safe-area.component.jsx
@@ -2,4 +2,6 @@ import { StatusBar, SafeAreaView } from "react-native";
 import styled from "styled-components/native";
 
 export const SafeArea = styled(SafeAreaView)`
-flex: 1; margin-top: ${StatusBar.currentHeight}px;`;
+flex: 1; margin-top: ${StatusBar.currentHeight}px;
+background-color: ${(props) => props.theme.colors.bg.primary};
+`;
